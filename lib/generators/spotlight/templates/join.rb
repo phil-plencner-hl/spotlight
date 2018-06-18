3ba5d42652dd4c382e69c31ef8d71bc55a103a3b
ives1227 <valdeva_crema@harvard.edu>
2018-06-18 07:13:00 -0400

    Adding breaks instead of commas for multiple values

--- lib/generators/spotlight/templates/join.rb
+++ lib/generators/spotlight/templates/join.rb
@@ -2,7 +2,7 @@ module Blacklight
   module Rendering
     class Join < AbstractStep
       def render
-        options = {:words_connector => "<br/>"}
+        options = {:words_connector => "<br/>", :two_words_connector => "<br/>", :last_word_connector => "<br/> "}
         next_step(values.map { |x| html_escape(x) }.to_sentence(options).html_safe)
       end
 
