b29308db000bdb0b54fb83a765d32d11d70195db
ives1227 <valdeva_crema@harvard.edu>
2018-11-19 14:05:59 -0500

    Prepping hldcp theme

--- lib/generators/spotlight/templates/config/initializers/spotlight_initializer.rb
+++ lib/generators/spotlight/templates/config/initializers/spotlight_initializer.rb
@@ -77,4 +77,4 @@
 # Spotlight::Engine.config.page_configurations = {
 #   'my-local-config': ->(context) { context.my_custom_data_path(context.current_exhibit) }
 # }
-Spotlight::Engine.config.exhibit_themes = %w[default nothumb]
+Spotlight::Engine.config.exhibit_themes = %w[default nothumb hldcp]
