c91f7ca5ffed581c0bc15d2ae2a10226e12e88c8
ives1227 <valdeva_crema@harvard.edu>
2019-02-12 21:09:13 -0500

    Removing duplicate translations

--- lib/generators/spotlight/install_generator.rb
+++ lib/generators/spotlight/install_generator.rb
@@ -197,9 +197,6 @@ module Spotlight
     def add_join
       copy_file 'join.rb', 'app/presenters/blacklight/rendering/join.rb'
     end
-    
-    def add_translations
-      copy_file 'config/initializers/translation.rb'
-    end
+ 
   end
 end
